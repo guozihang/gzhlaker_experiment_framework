86b431e51a67b4594591747cdf47d6993be0fc89
拉客的人 <15047271937@163.com>
2022-02-11 23:12:22 +0800

    1.3.37

--- core/log_manager.py
+++ core/log_manager.py
@@ -4,7 +4,7 @@ version:
 Author: Gzhlaker
 Date: 2022-02-11 22:17:25
 LastEditors: Andy
-LastEditTime: 2022-02-11 23:10:16
+LastEditTime: 2022-02-11 23:12:17
 '''
 import logging
 from rich.logging import RichHandler
@@ -17,10 +17,12 @@ class log_manager:
             level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
         )
         log = logging.getLogger("rich")
-
+        formatter = logging.Formatter(
+            "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+        )
         filehandler = logging.FileHandler(filename, "w")
         filehandler.setLevel(logging.NOTSET)
-        filehandler.setFormatter(" %(message)s")
+        filehandler.setFormatter(formatter)
         log.addHandler(filehandler)
-        
+
         return log
