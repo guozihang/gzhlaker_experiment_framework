760e15e5026c077cafe666d46172fcf5d7ec640b
拉客的人 <15047271937@163.com>
2022-02-11 23:08:14 +0800

    1.3.35

--- core/log_manager.py
+++ core/log_manager.py
@@ -4,17 +4,17 @@ version:
 Author: Gzhlaker
 Date: 2022-02-11 22:17:25
 LastEditors: Andy
-LastEditTime: 2022-02-11 23:02:52
+LastEditTime: 2022-02-11 23:08:07
 '''
 import logging
 from rich.logging import RichHandler
-
+from rich.console import Console
 class log_manager:
     @staticmethod
     def get_logger(filename):
         log = logging.getLogger("rich")
         log.setLevel(logging.NOTSET)
-        richhandler = RichHandler()
+        richhandler = RichHandler(console=Console(color_system = 'auto'))
         richhandler.setLevel(logging.NOTSET)
         richhandler.setFormatter(" %(message)s")
 
