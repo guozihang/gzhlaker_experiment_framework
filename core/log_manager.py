97546e0e71e443487833e6f476fc0de6965cf0a0
拉客的人 <15047271937@163.com>
2022-02-11 22:57:17 +0800

    1.3.31

--- core/log_manager.py
+++ core/log_manager.py
@@ -4,54 +4,23 @@ version:
 Author: Gzhlaker
 Date: 2022-02-11 22:17:25
 LastEditors: Andy
-LastEditTime: 2022-02-11 22:52:53
+LastEditTime: 2022-02-11 22:57:12
 '''
 import logging
-from unicodedata import name
 from rich.logging import RichHandler
 
 class log_manager:
-    level = None
-    formater = None
-    filehandler = None
     @staticmethod
     def get_logger(filename):
-        log_manager.set_level()
-        log_manager.set_fotmater()
-
-        logging.basicConfig(
-            level=logging.INFO,
-            format=log_manager.get_formater(), 
-            datefmt="[%X]", 
-            handlers=[RichHandler()],
-            # filename=filename,
-            # filemode="w"
-        )
-
         log = logging.getLogger("rich")
-        log.setLevel(log_manager.get_level())
+        log.setLevel(logging.INFO)
+        streamhandler = RichHandler()
+        streamhandler.setLevel(logging.INFO)
+        streamhandler.setFormatter("[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s")
+
+        filehandler = logging.FileHandler(filename=filename, filemode="w")
+        filehandler.setLevel(logging.INFO)
+        filehandler.setFormatter("[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s")
+
+        log.addHandler(log_manager.get_filehandler())
         return log
-    
-    @staticmethod
-    def set_level(level={
-            0: logging.DEBUG,
-            1: logging.INFO,
-            2: logging.WARNING,
-            3: logging.ERROR
-        }):
-        log_manager.level = level
-    
-    @staticmethod
-    def get_level():
-        return log_manager.level
-
-    @staticmethod
-    def set_fotmater(formater = "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"):
-        log_manager.formater = formater
-
-    @staticmethod
-    def get_formater():
-        return log_manager.formater
-        
-
-    
